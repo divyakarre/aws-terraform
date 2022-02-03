8ca4c8398db099c38b2992d9db7eba99586dff5a
divyakarre <karredivya@outlook.com>
2022-02-03 11:55:55 +0530

    commenting module files

--- main.tf
+++ main.tf
@@ -45,5 +45,28 @@ module "aws-vpc" {
 } */
 
 
+#### s3 Module ####
+ module s3 {
+  source = "./module/s3"
+  
+ }
 
+#### ec2 module ####
 
+  module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.12.0"
+
+  name           = var.vmname
+  instance_count = 1
+
+  ami                    = var.ami
+  instance_type          = var.instancetype
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
